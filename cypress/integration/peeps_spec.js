5420cdc170f834cd24f9c431022f7ddc9999d866
Caitlin Cooling <ccooling97@gmail.com>
2018-12-04 17:59:55 +0000

    Testing for an actual peep
    - Note that this will need to be stubbed eventually

--- cypress/integration/peeps_spec.js
+++ cypress/integration/peeps_spec.js
@@ -1,7 +1,5 @@
 describe("Peeps", function() {
-  it("shows the peeps on the page", function() {
-    cy.visit("http://localhost:8080/chitter.html")
-    cy.contains("View Peeps").click()
-    cy.contains("Peeps")
+  it("", function() {
+
   });
 });
