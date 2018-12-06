cee8da9eb3ea88f7b3e4a13168b872c1a2b40856
Caitlin Cooling <ccooling97@gmail.com>
2018-12-06 09:51:45 +0000

    PeepsRequest refactor

--- src/peepsRequest.js
+++ src/peepsRequest.js
@@ -1,16 +1,21 @@
 document.getElementById("getPeeps").addEventListener("click", getPeeps)
 
 function getPeeps () {
-  fetch('https://chitter-backend-api.herokuapp.com/peeps')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(peeps) {
+
+  function showPeeps(peeps) {
     for(var i = 0; i < peeps.length; i++){
       peep = document.createElement('div')
       peep.innerHTML = peeps[i].body
       document.getElementById("peeps").appendChild(peep)
     }
+  }
+
+  fetch('https://chitter-backend-api.herokuapp.com/peeps')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(peeps) {
+    showPeeps(peeps)
   })
   .catch(function(error) {
     console.error("Error", error)
